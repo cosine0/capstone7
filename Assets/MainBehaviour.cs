1178eb11c39241e444a534fa8572f99fce4435b5
Seok, Mi-seong <sjhwogml@naver.com>
2017-10-28 18:49:24 +0900

    dd

--- Assets/MainBehaviour.cs
+++ Assets/MainBehaviour.cs
@@ -32,7 +32,7 @@ public class MainBehaviour : MonoBehaviour
         // 테스트용 플레인 생성
         StartCoroutine(CreateTestPlanes());
 
-        
+        StartCoroutine(InsertMemberData());
     }
 
     private IEnumerator CreateTestPlanes()
@@ -221,7 +221,42 @@ public class MainBehaviour : MonoBehaviour
         }
     }
 
-    
+    IEnumerator InsertMemberData()
+    {
+        Debug.Log("Hi!");
+
+        //insert data to table
+        WWWForm form = new WWWForm();
+
+        string currlati = "37.450670";
+        string currlong = "126.656895";
+        string curralti = "123.123";
+
+        form.AddField("currlati", currlati);
+        form.AddField("currlong", currlong);
+        form.AddField("curralti", curralti);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://ec2-13-125-7-2.ap-northeast-2.compute.amazonaws.com:31337/capstone/getGPS_distance.php", form))
+        {
+            www.SetRequestHeader("Content-Type", "application/json");
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+                Debug.Log("Oh my god");
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+                Debug.Log(www.downloadHandler.text);
+
+                string a = www.downloadHandler.text;
+            }
+        }
+
+
+    }
 
     //IEnumerator GetPlaneList()
     //{
