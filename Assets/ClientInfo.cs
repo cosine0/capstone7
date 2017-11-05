9fd6c1365564afdd75e7e6403863c1c003cbe7ec
WDH <ha308162@naver.com>
2017-11-05 18:42:10 +0900

    ClientInfo 추가

---
+++ Assets/ClientInfo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ClientInfo
+{
+    public bool OriginalValuesSet = false;
+
+    public float StartingBearing = 0.0f;
+    public float StartingLatitude = 0.0f;
+    public float StartingLongitude = 0.0f;
+    public float StartingAltitude = 0.0f;
+
+    public float CurrentBearing = 0.0f;
+    public float CurrentLatitude = 0.0f;
+    public float CurrentLongitude = 0.0f;
+    public float CurrentAltitude = 0.0f;
+    public float LastGpsMeasureTime = 0.0f;
+
+    public GameObject MainCamera = null;
+}
