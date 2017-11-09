73412fa522308babd76a542cce1b9186814b6bda
WDH <ha308162@naver.com>
2017-11-09 16:53:22 +0900

    옵션 연결 완료

--- Assets/OptionBehaviour.cs
+++ Assets/OptionBehaviour.cs
@@ -11,8 +11,57 @@ public class OptionBehaviour : MonoBehaviour {
         _clientInfo = GameObject.FindGameObjectWithTag("ClientInfo").GetComponent<ClientInfo>();
         _userInfo = GameObject.FindGameObjectWithTag("UserInfo").GetComponent<UserInfo>();
 
-        GameObject.FindGameObjectWithTag("OptionUserId").GetComponent<Text>().text = "    " + _userInfo.UserName;
+        // User Id
+        GameObject.FindGameObjectWithTag("OptionUserId").GetComponent<Text>().text = "    " + _userInfo.UserId;
+        // User Point
+        GameObject.FindGameObjectWithTag("OptionUserPoint").GetComponent<Text>().text = "    " + _userInfo.Point.ToString();
 
+        // Inside option
+        GameObject offSwitch = GameObject.FindGameObjectWithTag("OptionInsideOff");
+        GameObject onSwitch = GameObject.FindGameObjectWithTag("OptionInsideOn");
+
+        if (_clientInfo.InsideOption)
+        {
+            // inside
+            offSwitch.GetComponent<CanvasGroup>().alpha = 0;
+            offSwitch.GetComponent<CanvasGroup>().interactable = false;
+            offSwitch.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+            onSwitch.GetComponent<CanvasGroup>().alpha = 1;
+            onSwitch.GetComponent<CanvasGroup>().interactable = true;
+            onSwitch.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+        else
+        {
+            // outside
+            offSwitch.GetComponent<CanvasGroup>().alpha = 1;
+            offSwitch.GetComponent<CanvasGroup>().interactable = true;
+            offSwitch.GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+            onSwitch.GetComponent<CanvasGroup>().alpha = 0;
+            onSwitch.GetComponent<CanvasGroup>().interactable = false;
+            onSwitch.GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+
+        // Distance option
+        switch (_clientInfo.DistanceOption)
+        {
+            case 1:
+                GameObject.FindGameObjectWithTag("OptionRadioButton").GetComponent<Distance_Radio>().meter_10.isOn = true;
+                break;
+            case 2:
+                GameObject.FindGameObjectWithTag("OptionRadioButton").GetComponent<Distance_Radio>().meter_20.isOn = true;
+                break;
+            case 3:
+                GameObject.FindGameObjectWithTag("OptionRadioButton").GetComponent<Distance_Radio>().meter_30.isOn = true;
+                break;
+            default:
+                Debug.Log("Distance Option Value Error");
+                break;
+        }
+
+        // Version Info
+        GameObject.FindGameObjectWithTag("OptionVersionInfo").GetComponent<Text>().text = _clientInfo.VersionInfo;
     }
 	
 	// Update is called once per frame
