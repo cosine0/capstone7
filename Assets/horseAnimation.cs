28b50d35c51797598eec45b5340087f614b50fa0
Seok, Mi-seong <sjhwogml@naver.com>
2017-11-12 16:47:39 +0900

    horse animation(curves, dopesheet), animator 수정, 애니메이션 부모object 생성

--- Assets/horseAnimation.cs
+++ Assets/horseAnimation.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class horseAnimation : MonoBehaviour
 {
-
-    //public Animator horseAnim;
-
     public GameObject obj;
+    //public Animator horseAnim;
 
     // Use this for initialization
     void Start()
@@ -20,15 +18,14 @@ public class horseAnimation : MonoBehaviour
     {
         //if (!horseAnim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.idle"))
         //{
-        //    Debug.Log("walk");
+        //    //Debug.Log("walk");
         //    //transform.position += new Vector3(Time.deltaTime, 0, 0);
-        //    //transform.Rotate(0, -0.5f, 0);
+        //    transform.Rotate(0, -0.5f, 0);
         //}
-
     }
 
     public void createObject() {
-        Instantiate(obj, new Vector3(15, -0.5835799f, 0.0f), Quaternion.identity);
+        Instantiate(obj, new Vector3(40, -1, 0.0f), Quaternion.identity);
     }
 
    
