5e0f181f28e93ae20ac6f9fa0060befab67ce624
Seok, Mi-seong <sjhwogml@naver.com>
2017-11-24 17:14:55 +0900

    option 변경중

--- Assets/viewCommentBehaviour.cs
+++ Assets/viewCommentBehaviour.cs
@@ -3,20 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class viewCommentBehaviour : MonoBehaviour {
+public class viewCommentBehaviour : MonoBehaviour
+{
+    public string opjName;
+    public string path;
+    public GameObject ScrollViewGameObject;
+    
+    
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        
+        //Cards is an array of data
+        opjName = "optionComment";
+        path = "Prefabs/" + opjName;
+        
+        for (int i = 0; i < 3; i++)
+        {
+            //ItemGameObject is my prefab pointer that i previous made a public property  
+            //and  assigned a prefab to it
+            GameObject card = Instantiate(Resources.Load(path)) as GameObject;
+
+            //scroll = GameObject.Find("CardScroll");
+            if (ScrollViewGameObject != null)
+            {
+                //ScrollViewGameObject container object
+                card.transform.SetParent(ScrollViewGameObject.transform, false);
+            }
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void ToOptionScene()
     {
         SceneManager.LoadScene("Option");
     }
+
+
+
 }
